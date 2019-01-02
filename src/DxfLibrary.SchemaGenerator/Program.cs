011ffa994831579bfc7b4c69cfd19bbd5e9d1711
Adam Renaud <adamf.renaud@gmail.com>
2019-01-01 21:23:39 -0500

    Working on the generator

---
+++ src/DxfLibrary.SchemaGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿// Program.cs
+// Created By: Adam Renaud
+// Created On: 2019-01-01
+
+// System Using Statements
+using System;
+using System.IO;
+
+// Internal Using Statements
+using Newtonsoft.Json.Schema;
+using DxfLibrary.DxfSpec;
+
+namespace DxfLibrary.SchemaGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new JsonSchemaGenerator();
+
+            var test = generator.Generate(typeof(IDxfSpec<object>));
+        }
+    }
+}

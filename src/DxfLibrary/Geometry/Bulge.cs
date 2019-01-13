09e6a60dab00f1687c765c5e926d2c96c474e1b1
Adam Renaud <adamf.renaud@gmail.com>
2019-01-13 15:45:36 -0500

    Adding Angles and Conversion function to Bulge

--- src/DxfLibrary/Geometry/Bulge.cs
+++ src/DxfLibrary/Geometry/Bulge.cs
@@ -2,6 +2,8 @@
 // By: Adam Renaud
 // Created on: 2019-01-13
 
+using System;
+
 namespace DxfLibrary.Geometry
 {
     /// <summary>
@@ -15,12 +17,29 @@ namespace DxfLibrary.Geometry
         /// <param name="bulgeValue">The value of the bulge</param>
         public Bulge(double bulgeValue)
         {
-            value = bulgeValue;
+            Value = bulgeValue;
         }
 
         /// <summary>
         /// The Value of the Bulge
         /// </summary>
-        public double value {get;}
+        public double Value {get;}
+
+        /// <summary>
+        /// The Angle of the Bulge
+        /// </summary>
+        public double Angle => Math.Atan(Value / 4);
+
+        /// <summary>
+        /// The Angle of the Bulge in Degrees
+        /// </summary>
+        public double AngleDeg => Rad2Deg(Angle);
+
+        /// <summary>
+        /// Converter for rads to degs
+        /// </summary>
+        /// <param name="val">Val in radians</param>
+        /// <returns>The value in degrees</returns>
+        private double Rad2Deg(double val) => val * (180 / Math.PI);
     }
 }

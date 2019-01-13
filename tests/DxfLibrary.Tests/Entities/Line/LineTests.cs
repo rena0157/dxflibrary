91de327a8de0546ce0e737562a69ad61a03aa793
Adam Renaud <adamf.renaud@gmail.com>
2019-01-12 22:29:57 -0500

    Line Tests

--- tests/DxfLibrary.Tests/Entities/Line/LineTests.cs
+++ tests/DxfLibrary.Tests/Entities/Line/LineTests.cs
@@ -11,6 +11,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 using DxfLibrary.Entities;
+using DxfLibrary.Geometry;
 
 namespace DxfLibrary.Tests.Entities
 {
@@ -55,17 +56,35 @@ ENDSEC
         #endregion
 
 
+        /// <summary>
+        /// The memory Stream that is used to simulate
+        /// a file that is read by the dxf file
+        /// </summary>
         private MemoryStream _memStream;
 
+        /// <summary>
+        /// A StreamWriter that is used to write to the memoryStream,
+        /// Note that this is not disposed of until the object is distroyed to 
+        /// keep the base stream alive.
+        /// </summary>
         private StreamWriter _writer;
 
         #region Constructors
 
+        /// <summary>
+        /// Default Constructor for the LineTests Class.
+        /// This constructor sets up everything that the test will need to run
+        /// </summary>
+        /// <param name="logger">A logger that is passed to the class during runtime</param>
         public LineTests(ITestOutputHelper logger) : base(logger)
         {
+            // Open the memory stream and write the data to it
             _memStream = new MemoryStream();
             _writer = new StreamWriter(_memStream);
             _writer.Write(testString);
+
+            // Make sure to flush the data and reset the position
+            // to simulate a file that is going to be read by the dxf file
             _writer.Flush();
             _memStream.Position = 0;
         }
@@ -74,16 +93,43 @@ ENDSEC
 
         #region TestMethods
 
+        /// <summary>
+        /// Quick test to make sure that the Get Entities function
+        /// is working
+        /// </summary>
         [Fact]
-        public void Test()
+        public void GetEntitiesTest()
         {
             DxfFile dxfFile = new DxfFile(_memStream);
+            var lines = dxfFile.GetEntities<Line>();
+            Assert.Equal(1, lines.Count());
+        }
+
+        /// <summary>
+        /// Test that the points are being read from the file correctly
+        /// </summary>
+        [Fact]
+        public void PointTest()
+        {
+            // Read the file and extract the lines
+            DxfFile dxfFile = new DxfFile(_memStream);
+            var lines = dxfFile.GetEntities<Line>();
+
+            // Set up two points and test to see if they match what was
+            // read from the file
+            Assert.Equal(new GeoPoint(6.820335402780456,6.189017678063052),
+                lines.FirstOrDefault().StartPoint);
+            Assert.Equal(new GeoPoint(15.86268769259617, 10.24974368590587),
+                 lines.FirstOrDefault().EndPoint);
         }
 
         #endregion
 
         #region Dispose Methods
 
+        /// <summary>
+        /// Dispose of the streams that were used in the file
+        /// </summary>
         public void Dispose()
         {
             _memStream.Dispose();

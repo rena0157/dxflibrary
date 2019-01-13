797b38325f1553736d0cb26d7794b2213f6ceb5e
Adam Renaud <adamf.renaud@gmail.com>
2019-01-13 10:04:21 -0500

    Adding Entity Tests

---
+++ tests/DxfLibrary.Tests/Entities/EntityTest.cs
@@ -0,0 +1,94 @@
+// EntityTest.cs
+// By: Adam Renaud
+// Created: 2019-01-13
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+using Xunit;
+using Xunit.Abstractions;
+
+using DxfLibrary.Entities;
+
+namespace DxfLibrary.Tests.Entities
+{
+    /// <summary>
+    /// Test basic <see cref="Entity"> parsing and functionality
+    /// </summary>
+    public class EntityTest : EntityTestBase
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="logger">Logger from XUnit</param>
+        public EntityTest(ITestOutputHelper logger) : base(logger)
+        {
+        }
+
+        #endregion
+
+        #region Tests
+
+        /// <summary>
+        /// Test that the layer name is being matched correctly
+        /// </summary>
+        [Theory]
+        [InlineData(_LayerNameTest, "TestLayer")]
+        public void LayerNameTest(string testString, string expected)
+        {
+            // Arrange
+            WriteMemory(testString);
+
+            // Act
+            DxfFile dxfFile = new DxfFile(_memoryStream);
+            var entity = dxfFile.Entities.First();
+
+            // Assert
+            Assert.Equal(expected, entity.LayerName);
+        }
+
+        #endregion
+
+        #region TestString
+
+        private const string _LayerNameTest = 
+@"  0
+SECTION
+  2
+ENTITIES
+  0
+LINE
+  5
+31B
+330
+1F
+100
+AcDbEntity
+  8
+TestLayer
+100
+AcDbLine
+ 10
+6.820335402780456
+ 20
+6.189017678063052
+ 30
+0.0
+ 11
+15.86268769259617
+ 21
+10.24974368590587
+ 31
+0.0
+  0
+ENDSEC
+  0";
+
+        #endregion
+    }
+}

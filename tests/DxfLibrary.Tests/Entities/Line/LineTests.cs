f95af2ad3acabfb25068786c57d1bbf615e481e4
Adam Renaud <adamf.renaud@gmail.com>
2019-01-13 09:51:41 -0500

    Adding Comments

--- tests/DxfLibrary.Tests/Entities/Line/LineTests.cs
+++ tests/DxfLibrary.Tests/Entities/Line/LineTests.cs
@@ -15,12 +15,16 @@ using DxfLibrary.Geometry;
 
 namespace DxfLibrary.Tests.Entities
 {
-    public class LineTests : TestBase, IDisposable
+    /// <summary>
+    /// Test class for the <see cref"Line"> type.
+    /// This test is only to test the parsing ability of the line
+    /// </summary>
+    public class LineTests : EntityTestBase
     {
 
-        #region TestString
+        #region TestStrings
 
-        private string testString = 
+        private string _TestString = 
 @"  0
 SECTION
   2
@@ -55,20 +59,6 @@ ENDSEC
 
         #endregion
 
-
-        /// <summary>
-        /// The memory Stream that is used to simulate
-        /// a file that is read by the dxf file
-        /// </summary>
-        private MemoryStream _memStream;
-
-        /// <summary>
-        /// A StreamWriter that is used to write to the memoryStream,
-        /// Note that this is not disposed of until the object is distroyed to 
-        /// keep the base stream alive.
-        /// </summary>
-        private StreamWriter _writer;
-
         #region Constructors
 
         /// <summary>
@@ -78,15 +68,8 @@ ENDSEC
         /// <param name="logger">A logger that is passed to the class during runtime</param>
         public LineTests(ITestOutputHelper logger) : base(logger)
         {
-            // Open the memory stream and write the data to it
-            _memStream = new MemoryStream();
-            _writer = new StreamWriter(_memStream);
-            _writer.Write(testString);
-
-            // Make sure to flush the data and reset the position
-            // to simulate a file that is going to be read by the dxf file
-            _writer.Flush();
-            _memStream.Position = 0;
+            // Write the string to memory
+            WriteMemory(_TestString);
         }
 
         #endregion
@@ -100,7 +83,7 @@ ENDSEC
         [Fact]
         public void GetEntitiesTest()
         {
-            DxfFile dxfFile = new DxfFile(_memStream);
+            DxfFile dxfFile = new DxfFile(_memoryStream);
             var lines = dxfFile.GetEntities<Line>();
             Assert.Equal(1, lines.Count());
         }
@@ -112,7 +95,7 @@ ENDSEC
         public void PointTest()
         {
             // Read the file and extract the lines
-            DxfFile dxfFile = new DxfFile(_memStream);
+            DxfFile dxfFile = new DxfFile(_memoryStream);
             var lines = dxfFile.GetEntities<Line>();
 
             // Set up two points and test to see if they match what was
@@ -121,19 +104,6 @@ ENDSEC
                 lines.FirstOrDefault().StartPoint);
             Assert.Equal(new GeoPoint(15.86268769259617, 10.24974368590587),
                  lines.FirstOrDefault().EndPoint);
-        }
-
-        #endregion
-
-        #region Dispose Methods
-
-        /// <summary>
-        /// Dispose of the streams that were used in the file
-        /// </summary>
-        public void Dispose()
-        {
-            _memStream.Dispose();
-            _writer.Dispose();
         }
 
         #endregion

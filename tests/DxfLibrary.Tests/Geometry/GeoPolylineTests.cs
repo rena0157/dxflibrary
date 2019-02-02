c1df7a98c15944a9b69663cd73d221f0b64834dc
Adam Renaud <adamf.renaud@gmail.com>
2019-02-01 20:36:59 -0500

    Adding tests for area for geopolyline

--- tests/DxfLibrary.Tests/Geometry/GeoPolylineTests.cs
+++ tests/DxfLibrary.Tests/Geometry/GeoPolylineTests.cs
@@ -3,6 +3,7 @@
 // 2019-01-27
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using DxfLibrary.Geometry;
 using Xunit;
@@ -22,6 +23,51 @@ namespace DxfLibrary.Tests.Geometry
         {
         }
 
+        #region TestData
+
+        /// <summary>
+        /// Class for holding the data for testing areas
+        /// </summary>
+        class AreaTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                // Type: Arc
+                // Draw Direction: CCW
+                // Bulge: +ve
+                yield return new object[]
+                {
+                    new GeoPolyline
+                    (
+                        new List<double>{ 0, 5, 5, 0 }, 
+                        new List<double>{ 0, 0, 2.5, 2.5 }, 
+                        new List<double>{ 0, 0, 1, 0}, 
+                        true
+                    ),
+                    22.3175
+                };
+
+                // Type: Arc
+                // Draw Direction: CW
+                // Bulge: -ve
+                yield return new object[]
+                {
+                    new GeoPolyline
+                    (
+                        new List<double>{ 0, 0, 5, 5 },
+                        new List<double>{ 0, 2.5, 2.5, 0 },
+                        new List<double>{ 0, -1.0, 0, 0 },
+                        true
+                    ),
+                    22.3175
+                };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        #endregion
+
         #region Tests
 
         [Fact]
@@ -40,6 +86,11 @@ namespace DxfLibrary.Tests.Geometry
             Assert.Equal(3, count);
         }
 
+        [Theory]
+        [ClassData(typeof(AreaTestData))]
+        public void AreaTests(GeoPolyline polyline, double expectedArea) => Assert.Equal(expectedArea, polyline.Area, 4);
+
         #endregion
+
     }
 }

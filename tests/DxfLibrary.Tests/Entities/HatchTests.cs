15704fdd6eb9726ade672484078fe2eeafb991e7
Adam Renaud <adamf.renaud@gmail.com>
2019-02-03 08:57:57 -0500

    Wiring the tests up with the data

--- tests/DxfLibrary.Tests/Entities/HatchTests.cs
+++ tests/DxfLibrary.Tests/Entities/HatchTests.cs
@@ -43,13 +43,17 @@ namespace DxfLibrary.Tests.Entities
 
         #region TestData
 
-        public class TestStringData : IEnumerable<object[]>
+        /// <summary>
+        /// Class Data for the Elevation Point Tests
+        /// </summary>
+        public class ElevationPointTestData : IEnumerable<object[]>
         {
+            // Regular Hatch Test
             public IEnumerator<object[]> GetEnumerator()
             {
                 yield return new object[]
                 {
-                    @"Test",
+                    RegularHatch,
                     new GeoPoint(0,0)
                 };
             }
@@ -61,14 +65,198 @@ namespace DxfLibrary.Tests.Entities
 
         #region Tests
 
+        /// <summary>
+        /// Testing the <see cref="Hatch.ElevationPoint"> Property Extraction
+        /// </summary>
+        /// <param name="testString">The Dxf String to test</param>
+        /// <param name="expectedValue">The expected value of the Elevation Point</param>
         [Theory]
-        [ClassData(typeof(TestStringData))]
+        [ClassData(typeof(ElevationPointTestData))]
         public void ElevationPointTest(string testString, GeoPoint expectedValue) 
             => Assert.Equal(expectedValue, GetFirstHatch(testString).ElevationPoint);
 
 
         #endregion
 
+
+        #region Regular Hatch
+
+        const string RegularHatch = 
+@"  0
+SECTION
+  2
+ENTITIES
+  0
+LWPOLYLINE
+  5
+323
+102
+{ACAD_REACTORS
+330
+331
+102
+}
+330
+1F
+100
+AcDbEntity
+  8
+TestLayer
+100
+AcDbPolyline
+ 90
+        4
+ 70
+     1
+ 43
+0.0
+ 10
+0.0
+ 20
+0.0
+ 10
+10.0
+ 20
+0.0
+ 10
+10.0
+ 20
+10.0
+ 10
+0.0
+ 20
+10.0
+  0
+HATCH
+  5
+331
+330
+1F
+100
+AcDbEntity
+  8
+TestLayer
+100
+AcDbHatch
+ 10
+0.0
+ 20
+0.0
+ 30
+0.0
+210
+0.0
+220
+0.0
+230
+1.0
+  2
+ANSI31
+ 70
+     0
+ 71
+     1
+ 91
+        1
+ 92
+        1
+ 93
+        4
+ 72
+     1
+ 10
+0.0
+ 20
+0.0
+ 11
+10.0
+ 21
+0.0
+ 72
+     1
+ 10
+10.0
+ 20
+0.0
+ 11
+10.0
+ 21
+10.0
+ 72
+     1
+ 10
+10.0
+ 20
+10.0
+ 11
+0.0
+ 21
+10.0
+ 72
+     1
+ 10
+0.0
+ 20
+10.0
+ 11
+0.0
+ 21
+0.0
+ 97
+        1
+330
+323
+ 75
+     1
+ 76
+     1
+ 52
+0.0
+ 41
+4.999999999999998
+ 77
+     0
+ 78
+     1
+ 53
+45.0
+ 43
+0.0
+ 44
+0.0
+ 45
+-0.4419417382415921
+ 46
+0.4419417382415921
+ 79
+     0
+ 98
+        1
+ 10
+0.0
+ 20
+0.0
+1001
+GradientColor1ACI
+1070
+     5
+1001
+GradientColor2ACI
+1070
+     2
+1001
+ACAD
+1010
+0.0
+1020
+0.0
+1030
+0.0
+  0
+ENDSEC
+  0";
+
+        #endregion
 
     }
 }

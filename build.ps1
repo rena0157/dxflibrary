78ebded77c55f4c31d1575987ea1244233d07db8
Adam Renaud <adamf.renaud@gmail.com>
2019-02-03 18:01:23 -0500

    Adding a build Task

---
+++ build.ps1
@@ -0,0 +1,12 @@
+# build.ps1
+# By: Adam Renaud
+# Created on: 2019-02-03
+
+Write-Output "Building All Projects"
+
+# The Solution Dir
+$SolutionDir = "C:\Dev\dxflibrary"
+$ArtifactsDir = Join-Path -Path $SolutionDir -ChildPath "artifacts"
+
+# Build the projects and place into artifacts
+(Get-ChildItem -Path $SolutionDir -Recurse -Filter "*.csproj").FullName | dotnet build -o $ArtifactsDir

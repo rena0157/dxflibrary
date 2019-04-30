5cd51524a9e6198069181ffa65b825f0a61340c4
Adam Renaud <adamf.renaud@gmail.com>
2019-04-29 22:52:48 -0400

    Adding the Sadbox application

---
+++ tests/Sandbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello World";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World");
+            Thread.Sleep(5000);
+        }
+    }
+}

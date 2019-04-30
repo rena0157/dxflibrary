eef756b0f012746d0c4d22b8c6862e1be74ed284
Adam Renaud <adamf.renaud@gmail.com>
2019-04-29 22:38:38 -0400

    Adding Comments

--- src/DxfLibrary/IO/DxfBinaryReader.cs
+++ src/DxfLibrary/IO/DxfBinaryReader.cs
@@ -7,17 +7,29 @@ using System.IO;
 
 namespace DxfLibrary.IO
 {
+    /// <summary>
+    /// Dxf Binary Reader class
+    /// </summary>
+    /// <typeparam name="byte[]"></typeparam>
+    /// <typeparam name="byte[]"></typeparam>
     public class DxfBinaryReader : IDxfReader<byte[], byte[]>
     {
 
         #region Private Properties
 
+        /// <summary>
+        /// The Binary Reader stream
+        /// </summary>
         private BinaryReader _reader;
 
         #endregion
 
         #region Constructors
 
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="stream">A base stream passed into the constructor</param>
         public DxfBinaryReader(Stream stream)
         {
             _reader = new BinaryReader(stream);
@@ -27,21 +39,35 @@ namespace DxfLibrary.IO
 
         #region Public Properties
 
+        /// <summary>
+        /// The Path to the file from the base stream
+        /// </summary>
         public string Path => (_reader.BaseStream as FileStream).Name;
 
+        /// <summary>
+        /// The Current Position in the stream
+        /// </summary>
         public long CurrentPosition {get; protected set;}
 
+        /// <summary>
+        /// The end of the stream
+        /// </summary>
         public bool EndOfStream => _reader.BaseStream.Position == _reader.BaseStream.Length;
 
         #endregion
 
         #region Public Methods
 
-        public void Dispose()
-        {
-            _reader.Dispose();
-        }
+        /// <summary>
+        /// Public Dispose method because this class
+        /// inherits the IDisposable interface
+        /// </summary>
+        public void Dispose() => _reader.Dispose();
 
+        /// <summary>
+        /// Get the next pair of data from the stream
+        /// </summary>
+        /// <returns>Returns the data for the next pair</returns>
         public TaggedData<byte[], byte[]> GetNextPair()
         {
             return new TaggedData<byte[], byte[]>(_reader.ReadBytes(2), _reader.ReadBytes(2));

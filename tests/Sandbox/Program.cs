c7f6dd36f90abc26b98aca583bb109e78760886d
Adam Renaud <adamf.renaud@gmail.com>
2019-04-30 18:36:57 -0400

    Adding test file to the SandBox

--- tests/Sandbox/Program.cs
+++ tests/Sandbox/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-using System.Threading;
+using System.IO;
+using DxfLibrary;
+using DxfLibrary.IO;
 
 namespace Sandbox
 {
     class Program
     {
+        private static string _dirname = @"C:\Dev\dxfTestData\";
+
         static void Main(string[] args)
         {
-            Console.Title = "Hello World";
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Hello World");
-            Thread.Sleep(5000);
+            Log("Starting Sandbox");
+
+            var testFile = Path.Join(_dirname, "BinTest.dxf");
+            Log($"Opening File {testFile}");
+
+            using(var stream = new FileStream(testFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var dxfFile = new DxfFile(stream, true);   
+            }
+
+        }
+
+        private static void Log(object message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] -- {message.ToString()}");
         }
     }
 }
